159f664b244d92a768bb7399ee042b186ff15f5c
Yoanvari <yoanvari123@gmail.com>
2025-01-16 22:53:17 +0700

    edit index with native stack navigator

--- app/index.js
+++ app/index.js
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { AuthProvider, useAuth } from "./(auth)/authContext";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import login from "./(auth)/login";
+import register from "./(auth)/register";
+import home from "./(index)/home";
+
+const Stack = createNativeStackNavigator();
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <AuthProvider>
+      <Layout></Layout>
+    </AuthProvider>
   );
 }
+
+export const Layout = () => {
+  const { authState, onLogout } = useAuth();
+
+  return (
+    <Stack.Navigator>
+      {authState?.authenticated ? (
+        <Stack.Screen
+          name="Home"
+          component={home}
+          options={{
+            headerRight: () => <Button onPress={onLogout} title="Sign Out" />,
+          }}
+        ></Stack.Screen>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={login} />
+          <Stack.Screen name="Register" component={register} />
+          <Stack.Screen name="Home" component={home} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
