faffca652c615ddf75de735985b65960d845f748
Yoanvari <yoanvari123@gmail.com>
2025-01-16 22:54:46 +0700

    add login page with logic and ui

---
+++ app/(auth)/login.js
@@ -0,0 +1,124 @@
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { useAuth } from "./authContext";
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { onLogin } = useAuth();
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      Alert.alert("Error", "All fields are required!");
+      return;
+    }
+
+    // Proses login
+    try {
+      const result = await await onLogin(email, password);
+      console.log("login result:", result);
+
+      if (result?.error) {
+        Alert.alert("Error", result.msg || "Failed to register.");
+        return;
+      }
+
+      Alert.alert("Success", "Account created successfully!");
+      navigation?.navigate("Home");
+    } catch (error) {
+      Alert.alert("Error", "Something went wrong. Please try again.");
+      console.error(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome Back!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#aaa"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#aaa"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.signInButton} onPress={handleLogin}>
+        <Text style={styles.signInButtonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text
+          style={styles.createAccountText}
+          onPress={() => navigation.navigate("Register")}
+        >
+          Create Account
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f9f9f9",
+    paddingBottom: 200,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 30,
+    color: "#333",
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    fontSize: 16,
+    color: "#333",
+    backgroundColor: "#fff",
+  },
+  signInButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#007bff",
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  signInButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  createAccountText: {
+    fontSize: 16,
+    color: "#007bff",
+    textDecorationLine: "underline",
+  },
+});
+
+export default Login;
